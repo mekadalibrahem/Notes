0eb52a422abccd25954b4079b06c0289867c1be9
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-07-19 16:07:46 +0300

    init project and add configration file

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php  
+return [
+    'app_name' => 'Notes',
+    'host' => '127.0.0.4',
+    "database" =>[
+        'driver' => "mysql" ,
+        'dsn' => [
+            'host' => "127.0.0.1",
+            'port' => 3306,
+            "dbname" => "notes_db" ,
+            'charset' => 'utf8mb4'
+
+        ],
+        'username' => 'root',
+        'password' => ''
+    ],
+];

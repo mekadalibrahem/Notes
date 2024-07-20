5ffdcacc87b33ab64374b5286f11bd0559d88edf
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-07-20 23:52:21 +0300

    fix call method

--- controllers/notes.php
+++ controllers/notes.php
@@ -6,7 +6,6 @@ $db = new Database($config['database']);
 
 $notes = [] ; 
 $user_id = $_GET['id'];
-$query_result = $db->query("SELECT * FROM `notes` WHERE user_id = ?" , [$user_id]) ;
-$notes = $query_result->fetchAll(PDO::FETCH_ASSOC);
+$notes = $db->query("SELECT * FROM `notes` WHERE user_id = ?" , [$user_id])->get();
 
 require "./views/notes.view.php";

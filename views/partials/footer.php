83636cb5ddd9645fb53eed96ddc9b54328a2cc75
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-07-21 15:26:17 +0300

    replace flowbit lib for desing with tillwindcss

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,7 +1,6 @@
 <footer>
 
 </footer>
-<script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
 
 </body>
 
--- views/partials/head.php
+++ views/partials/head.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?> - <?= $config['app_name'] ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
 

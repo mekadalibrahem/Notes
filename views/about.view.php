b073681119b35687ed7a2a8cd166b9181fa3a151
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-07-21 15:48:34 +0300

    edit main section  style

--- views/about.view.php
+++ views/about.view.php
@@ -3,7 +3,7 @@
 require "partials/head.php"; 
 require "partials/nav.php"; 
 ?>
-<main class="max-w-screen-xl mx-auto p-4 " > 
+<main class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"> 
 
 <h2>   Hello in About page </h2>
 
--- views/notes.view.php
+++ views/notes.view.php
@@ -3,19 +3,16 @@
 require "partials/head.php"; 
 require "partials/nav.php"; 
 ?>
-<main class="max-w-screen-xl mx-auto p-4 " > 
+<main class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"> 
 
 <h2>   Hello in Notes page </h2>
 
-<h3>Your Notes</h3>
+<h3>Your Notes : </h3>
 
 
 
 <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-    <!-- <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">Profile</li>
-    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">Settings</li>
-    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">Messages</li>
-    <li class="w-full px-4 py-2 rounded-b-lg">Download</li> -->
+
     <?php foreach($notes as $note): ?>
         <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
             <a href="/note?id=<?= $note['id'] ?>">
@@ -26,11 +23,6 @@ require "partials/nav.php";
        <?php endforeach ; ?>
 </ul>
 
-
-<div class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-    
-   
-</div>
 
 
 

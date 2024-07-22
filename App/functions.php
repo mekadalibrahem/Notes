a0a5b9009ccd3d8ab664b98ecdeba81f7559d5b3
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-07-22 18:55:27 +0300

    add method config for return config by value

--- App/functions.php
+++ App/functions.php
@@ -50,3 +50,28 @@ function setRequestMethod($method = "GET"){
     $method = strtoupper(trim($method));
     return "<input type='hidden' name='__method' value='$method' />";
 }
+
+
+/**
+ * return value form config file
+ * @param mixed $key key for value which will return
+ * @return string|Exception|null value or throwig Exeption
+ */
+function config($key){
+    $config =  require  base_path("config.php");
+    $value = null;
+    // convert string to array by spicifc char
+    $split_key =   explode(".", $key);
+   
+    foreach ($split_key as $i) {
+        if(! array_key_exists($i , $config)) {
+            throw new \Exception("Key not found  : {$key} ");
+        }else{
+            $value = $config[$i];
+            $config = $value;
+          
+        }
+        
+    }
+    return $value ;
+}

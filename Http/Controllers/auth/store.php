05436cc2099ef9e2b1ec1d4e6bd4fd43d7e6e9b5
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-07-25 23:24:48 +0300

    extract validate form login for own class

--- Http/Controllers/auth/store.php
+++ Http/Controllers/auth/store.php
@@ -2,46 +2,28 @@
 
 use App\Container;
 use App\Database;
-use App\Validator;
 
+use Http\Forms\LoginForm;
+$db = Container::resolve(Database::class);
 $email = $_POST['email'] ?? false;
 $password = $_POST['password'] ?? false;
-$errors = [];
-$db = Container::resolve(Database::class);
-if(!Validator::email($email)) {
-    $errors["email"] = 'Invalid email address';
-}
-if(!Validator::string($password, 5,50)) {
-    $errors['password'] = 'password is required and should be 5-50 charachters';
-}
-if(!empty($errors)){
-    view("auth/create.view.php", 
-    [
-        "errors"=> $errors,
-       "page_title" => "Login",
-    
-    ]);
-    die();
-}
-$user =    $db->query("SELECT * FROM users WHERE email = :email",["email" =>$email])->find();
 
-if(!$user){
-    $errors["email"] = "Wrong email dosn't esists";
-}else{
-    if(!password_verify($password,$user['password'])){
-        $errors['password'] = "Wrong Password"; 
-    }
-      
-}
-if(!empty($errors)){
+$form = new LoginForm();
+
+if(!$form->validate($email, $password)){
     view("auth/create.view.php", 
     [
-        "errors"=> $errors,
+        "errors"=> $form->errors(),
         "page_title" => "Login",
     ]);
     die();
+}else{
+    $db = Container::resolve(Database::class);
+    $user = $db->query("SELECT * FROM users WHERE email= :email ", ["email"=>$email])->find();
+    login($user['id']);
+    header('location: /notes');
 }
+       
 
-login($user['id']);
-header('location: /notes');
+
 
---
+++ Http/Forms/LoginForm.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace Http\Forms;
+use App\Container;
+use App\Database;
+use App\Validator;
+
+class LoginForm {
+    protected $errors = [] ;
+
+    public function validate($email , $password) {
+        $this->errors = [] ;
+        $user  = false ;
+        $db = Container::resolve(Database::class);
+
+        if(!Validator::string($password, 5,50)) {
+            $this->errors['password'] = 'password is required and should be 5-50 charachters';
+        }
+        if(!Validator::email($email)) {
+            $this->errors["email"] = 'Invalid email address';
+        }else{
+            $user =    $db->query("SELECT * FROM users WHERE email = :email",["email" =>$email])->find();
+
+            if(!$user){
+                $this->errors["email"] = "Wrong email dosn't esists";
+            }else{
+                if(!empty($password)){
+                    if(!password_verify($password,$user['password'])){
+                        $this->errors['password'] = "Wrong Password"; 
+                    }
+                }
+            }
+        }
+       
+       
+       
+    
+        
+        
+        return empty($this->errors) ;
+    }
+
+    public function errors(){
+        return $this->errors ;
+    }
+
+}

fcb04eb6d1492e03d5115429fbc267a1253c0f91
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-07-26 11:36:00 +0300

    extract auth user method for Authenticator class

---
+++ App/Authenticator.php
@@ -0,0 +1,40 @@
+<?php 
+namespace App;
+
+class Authenticator {
+
+    public function attemp($email , $password) {
+        if($this->cardinate_verify( $email , $password )) {
+            $this->login( $email );
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public  function login($email){
+        Session::put("auth_user_email" , $email);
+    }
+
+    public function logout(){
+        Session::destroy();
+    }
+
+    protected function cardinate_verify($email , $password){
+        $db = Container::resolve(Database::class);
+        $user = $db->query("SELECT * FROM users WHERE email = :email" ,["email" =>$email])->find();
+        return password_verify($password , $user["password"]);
+    }
+
+    public function auth(){
+        return Session::has('auth_user_email') ;
+    }
+
+    public function user(){
+        $email = $this->auth();
+        $db= Container::resolve(Database::class);
+        $user = $db->query("SELECT * FROM users WHERE email = :email" ,["email" =>$email])->find();
+        return $user ;
+    }
+    
+}
